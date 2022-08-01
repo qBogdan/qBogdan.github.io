04c54c80b0bca12d4ac85acb413654a82bcc64a6
Ghițun Iulian Bogdan <ghitun.iulian@yahoo.com>
2022-08-01 20:36:01 +0300

    added skills objects on cv2

--- cv 2/script.js
+++ cv 2/script.js
@@ -16,3 +16,63 @@ window.addEventListener('scroll', (e) => {
         }
     })
 })
+
+// let options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.8
+// }
+
+// let observer = new IntersectionObserver(cb, options);
+
+// function cb(pages, observer) {
+//     pages.forEach(page => {
+//         observer.observe(page);
+//     })
+// }
+
+/* -------------------- */
+
+const skills = [
+    {
+        name: "HTML",
+        lvl: 10,
+    },
+    {
+        name: "CSS",
+        lvl: 10,
+    },
+    {
+        name: "JavaScript",
+        lvl: 8,
+    },
+    {
+        name: "React",
+        lvl: 0,
+    }
+]
+
+skills.forEach(skill => {
+    let li = document.createElement('li')
+    let h2 = document.createElement('h3')
+    let skillLvl = document.createElement('div');
+    skillLvl.classList.add('skillLvl')
+
+    h2.innerText = skill.name;
+
+    for (let i = 0; i < 10; i++) {
+        let div = document.createElement('div');
+        div.classList.add('skillPoint');
+        skillLvl.append(div)
+
+        if (i < skill.lvl) {
+            div.classList.add('activePoint')
+        }
+    }
+
+    li.append(h2);
+    li.append(skillLvl);
+
+    document.getElementById('skillList').append(li);
+
+})
